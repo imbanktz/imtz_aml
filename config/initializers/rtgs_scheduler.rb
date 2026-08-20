ad07c5fc21038a69c6b8a85451bd40c4b33d177e
abdimuna <abdimuna1@gmail.com>
2026-08-20 11:35:29 +0300

    commented the scheduler

--- config/initializers/rtgs_scheduler.rb
+++ config/initializers/rtgs_scheduler.rb
@@ -1,15 +1,15 @@
-# config/schedule.rb
-require 'whenever'
+# # config/schedule.rb
+# require 'whenever'
 
-set :environment, Rails.env
+# set :environment, Rails.env
 
-# Process RTGS files every 3 minutes
-every 3.minutes do
-  runner "ProcessRtgsWithManualParserJob.perform_now"
-  command "echo 'RTGS processing triggered at $(date)' >> log/rtgs_scheduler.log"
-end
-
-# For testing, run every minute
-# every 1.minute do
+# # Process RTGS files every 3 minutes
+# every 3.minutes do
 #   runner "ProcessRtgsWithManualParserJob.perform_now"
+#   command "echo 'RTGS processing triggered at $(date)' >> log/rtgs_scheduler.log"
 # end
+
+# # For testing, run every minute
+# # every 1.minute do
+# #   runner "ProcessRtgsWithManualParserJob.perform_now"
+# # end
