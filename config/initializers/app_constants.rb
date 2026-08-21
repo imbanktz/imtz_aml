0a950797627f9f06bea5aa4213360ff203bcb120
abdillah muna <abdillah.muna@imbank.co.tz>
2026-08-21 10:13:50 -0400

    changed production amlock IP

--- config/initializers/app_constants.rb
+++ config/initializers/app_constants.rb
@@ -6,7 +6,7 @@ API_TRANSACTION_SCREENING = "/screening/transaction/generic/check"
 API_CUSTOMER_SCREENING = "/screening/customer/check"
 TOKEN_ENDPOINT = '/security/accessToken'
 # Amlock
-AMLOCK_SERVER_IP = ENV['AMLOCK_SERVER_IP'] || '192.168.205.136'
+AMLOCK_SERVER_IP = ENV['AMLOCK_SERVER_IP'] || '192.168.201.136'
 AMLOCK_USER_NAME = ENV['AMLOCK_USER_NAME'] || 'amladm'
 AMLOCK_PASSWORD = ENV['AMLOCK_PASSWORD'] || '123qweASD!@#'
 AMLOCK_SOURCE_FILES = ENV['AMLOCK_SOURCE_FILES'] || '/amlock/Tanzania/RMS/mxt_to_mt/rtgs_source_only'
